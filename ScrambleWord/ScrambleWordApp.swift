b311f52926dae3e57960a8e42e68161250db46da
Mete Alp Kızılçay <kizilcaymetealp@gmail.com>
2020-10-19 22:44:07 +0300

    Initial Commit

---
+++ ScrambleWord/ScrambleWordApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrambleWordApp.swift
+//  ScrambleWord
+//
+//  Created by Mete Alp Kizilcay on 19.10.2020.
+//
+
+import SwiftUI
+
+@main
+struct ScrambleWordApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
